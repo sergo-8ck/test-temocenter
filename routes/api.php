00f512b0ae4e4bbc47c3dea3f04e6d6190c6529c
skarpov <sergo-8ck@yandex.ru>
2019-08-01 15:56:08 +0300

    Create routes

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,14 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+//Route::middleware('auth:api')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
+
+Route::prefix('/articles')->group(function() {
+    Route::get('/', ['uses' => 'ArticleController@get']);
+    Route::get('/{article_id}', ['uses' => 'ArticleController@detail'])->where(['article_id' => '[0-9+]']);
+    Route::post('/', ['uses' => 'ArticleController@create']);
+    Route::delete('/{article_id}', ['uses' => 'ArticleController@delete'])->where(['article_id' => '[0-9+]']);
+    Route::put('/{article_id}', ['uses' => 'ArticleController@update'])->where(['article_id' => '[0-9+]']);
 });
