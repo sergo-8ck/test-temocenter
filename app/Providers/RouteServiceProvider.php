88f1b35c7ff8bf46038c0a78871abe9f65ac09ca
skarpov <sergo-8ck@yandex.ru>
2019-08-01 18:40:05 +0300

    -R-- public

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -66,7 +66,6 @@ class RouteServiceProvider extends ServiceProvider
     protected function mapApiRoutes()
     {
         Route::prefix('api/v1')
-             ->middleware('api')
              ->namespace($this->namespace)
              ->group(base_path('routes/api.php'));
     }
--- routes/api.php
+++ routes/api.php
@@ -20,7 +20,7 @@ use Illuminate\Http\Request;
 Route::prefix('/articles')->group(function() {
     Route::get('/', ['uses' => 'ArticleController@get']);
     Route::get('/{article_id}', ['uses' => 'ArticleController@detail'])->where(['article_id' => '[0-9+]']);
-    Route::post('/', ['uses' => 'ArticleController@createArticle']);
-    Route::delete('/{article_id}', ['uses' => 'ArticleController@delete'])->where(['article_id' => '[0-9+]']);
-    Route::put('/{article_id}', ['uses' => 'ArticleController@updateArticle'])->where(['article_id' => '[0-9+]']);
+    Route::post('/', ['uses' => 'ArticleController@createArticle'])->middleware('api');
+    Route::delete('/{article_id}', ['uses' => 'ArticleController@delete'])->where(['article_id' => '[0-9+]'])->middleware('api');
+    Route::put('/{article_id}', ['uses' => 'ArticleController@updateArticle'])->where(['article_id' => '[0-9+]'])->middleware('api');
 });
